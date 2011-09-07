e63e505248cc338e64f32635ae5fb77aece14d39
whired <whired@debian>
2011-09-07 15:16:43 -0400

    Integrate updater base Add dist

--- src/org/whired/ghostclient/Main.java
+++ src/org/whired/ghostclient/Main.java
@@ -5,7 +5,7 @@ import org.whired.ghostclient.ui.GhostUserImpl;
 
 public class Main
 {
-	public static void main(String args[]) 
+	public static void main(String args[])
 	{
 		new GhostUserImpl(new GhostFrameImpl());
 	}
